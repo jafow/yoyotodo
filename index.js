c3bef9fe1a2c0784ac50d80c0e0dad45691fe455
jafow <jared.a.fowler@gmail.com>
2017-07-10 19:47:36 -0700

    Add todos \o/

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
+const yo = require('yo-yo')
 var addBtn = document.getElementById('add-todo')
 var todoInput = document.getElementById('input-todo')
-addBtn.addEventListener('click', addTodo)
+var container = document.getElementById('list')
+var todoList = new TodoList()
+addBtn.addEventListener('click', todoList.addTodo)
+document.addEventListener('DOMContentReady', todoList.update)
 
 class TodoList {
   constructor() {
     this.items = []
+    this.addTodo = this.addTodo.bind(this)
+    this.formatItem = this.formatItem.bind(this)
+    this.update = this.update.bind(this)
   }
   addTodo() {
-    formatItem(getInput(todoInput))
-    renderItem()
-
+    this.items.push(this.formatItem(getInput(todoInput)))
+    yo.update(container, this.update())
   }
   formatItem(str) {
     return {
       id: this.items.length,
-      item: str
+      content: str
     }
   }
-
+  update() {
+   return yo`
+     <div class="items-list">
+      <ul>
+        ${this.items.map(item => yo`<li data-id=${item.id}>${item.content}</li>`)}
+       </ul>
+     </div>`
+  }
 }
 
 function getInput (el) {
